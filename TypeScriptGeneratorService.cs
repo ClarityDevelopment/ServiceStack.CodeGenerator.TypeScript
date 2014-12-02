5c5019e452999d15381bc3e6fa9f26121f062a2b
Noah Shipley <Noah.Shipley@ClarityMIS.com>
2014-12-02 10:46:59 -0600

    Removed old csproj. Added TypeScriptGeneratorService.cs

---
+++ TypeScriptGeneratorService.cs
@@ -0,0 +1,45 @@
+﻿namespace ServiceStack.CodeGenerator.TypeScript
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    using ServiceStack;
+
+    [Route("/CodeGen/", "GET", Summary = @"Generates typescript from our routes.")]
+    public class CodeGenRoute : IReturn<string>
+    {
+        [ApiMember(IsRequired = false)]
+        public string TypeNamePattern { get; set; }
+
+        [ApiMember(IsRequired = false)]
+        public string ClrNamespace { get; set; }
+    }
+
+
+    public class CodeGenService : Service
+    {
+        public string Any(CodeGenRoute codeGen)
+        {
+            // http://localhost/service/CodeGen?TypeNamePattern=GetShipments
+            var routeTypes =
+                AppDomain.CurrentDomain.GetAssemblies()
+                .Where(a => a.FullName.StartsWith(string.IsNullOrEmpty(codeGen.ClrNamespace) ? "Clarity.Ecommerce.Service" : codeGen.ClrNamespace))
+                    .SelectMany(
+                        a =>
+                        a.GetTypes()
+                            .Where(
+                                t => t.CustomAttributes.Any(attr => attr.AttributeType == typeof(RouteAttribute))));
+
+            if (!string.IsNullOrEmpty(codeGen.TypeNamePattern))
+            {
+                var r = new Regex(codeGen.TypeNamePattern);
+                routeTypes = routeTypes.Where(rt => r.Match(rt.Name).Success);
+            }
+
+            var cg = new TypescriptCodeGenerator(routeTypes, "cv.cef.api", new string[] { "Clarity.Ecommerce.DataModel" }, "$cef");
+            return cg.GenerateClient() + "\n" + cg.GenerateDtos() + "\n" + cg.GenerateRoutes();
+        }
+
+    }
+}
