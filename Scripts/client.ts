aab594cd26c9ce00e6ad555d9a18e7700cc12db1
Noah Shipley <Noah.Shipley@ClarityMIS.com>
2015-01-09 04:43:30 -0600

    Added inheritance support for DTOs

--- Scripts/client.ts
+++ Scripts/client.ts
@@ -2,22 +2,112 @@
  
  
  
+// ----- DTOS -----
+    export interface SomeDto {
+        ID: number;
+        Value?: string;
+    }
+
+
+   
+    // ----- Routes -----
+    
+    /**
+    * Base class for classes implementing communication with a service stack route.
+    */
+    export class RouteAggregator {
+        public service : cef;
+        get rootUrl() : string { return this.service.rootUrl; }
+        get $http() : ng.IHttpService { return this.service.$http; }
+
+        constructor(service: cef) {
+            this.service = service;            
+        }
+    }
+
+    export interface ReturnStringWithParamDto {
+        Flag: boolean;
+    }
+    export interface RouteWithDTODto {
+        // API Member Description about Flag
+        Flag: boolean;
+    }
+    export interface RouteWithParamDto {
+        // API Member Description about Flag
+        Flag: boolean;
+    }
+
+        export class Test extends RouteAggregator {
+        /**
+         * C# Type:  ServiceStack.CodeGenerator.TypeScript.Tests.ReturnString
+         * Path: /Test/ReturnString
+        */
+        ReturnString = () => {
+            return this.$http<string>({
+                url:  [this.rootUrl, "Test", "ReturnString"].join('/'),
+                method: 'GET',
+            });
+        }
+        
+        /**
+         * C# Type:  ServiceStack.CodeGenerator.TypeScript.Tests.ReturnStringWithParam
+         * Path: /Test/ReturnStringWithParam
+        */
+        ReturnStringWithParam = (routeParams : ReturnStringWithParamDto) => {
+            return this.$http<string>({
+                url:  [this.rootUrl, "Test", "ReturnStringWithParam"].join('/'),
+                method: 'GET',
+                params:  routeParams
+            });
+        }
+        
+        /**
+         * C# Type:  ServiceStack.CodeGenerator.TypeScript.Tests.RouteWithDTO
+         * Path: /Test/{ID}/ReturnDTO
+        */
+        RouteWithDTO = (id: number, routeParams : RouteWithDTODto) => {
+            return this.$http<SomeDto>({
+                url:  [this.rootUrl, "Test", id, "ReturnDTO"].join('/'),
+                method: 'GET',
+                params:  routeParams
+            });
+        }
+        
+        /**
+         * C# Type:  ServiceStack.CodeGenerator.TypeScript.Tests.RouteWithParam
+         * Path: /Test/{ID}/ReturnVoid
+        */
+        RouteWithParam = (id: number, routeParams : RouteWithParamDto) => {
+            return this.$http<void>({
+                url:  [this.rootUrl, "Test", id, "ReturnVoid"].join('/'),
+                method: 'GET',
+                params:  routeParams
+            });
+        }
+        
+        /**
+         * C# Type:  ServiceStack.CodeGenerator.TypeScript.Tests.VoidReturnRoute
+         * Path: /Test/ReturnVoid
+        */
+        VoidReturnRoute = () => {
+            return this.$http<void>({
+                url:  [this.rootUrl, "Test", "ReturnVoid"].join('/'),
+                method: 'GET',
+            });
+        }
+        
+    }
+
+
+
+
 ///<reference path="_references.ts"/>
 
-///<reference path="dtos.ts"/>
-///<reference path="routes.ts"/>
 
-module cv.cef.api {
-    angular
-        .module('cv.cef.api', [])
-        .service('$cef', ['$http', ($http : ng.IHttpService) => {
-            return new Client($http);
-        }]);
-    
     /**
      * Exposes access to the ServiceStack routes
     */
-    export class Client {
+    export class cef {
         
         private _$http: ng.IHttpService;
         get $http():ng.IHttpService {
@@ -40,12 +130,11 @@ module cv.cef.api {
         constructor($http: ng.IHttpService) {
             this._$http = $http;
 
-            this.test = new routes.Test(this);
+            this.test = new Test(this);
         }
 
-        test: routes.Test
+        test: Test;
 
     }
 
-}
 
